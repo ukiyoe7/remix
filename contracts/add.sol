623121e66ed424c3545dcee64314fa77b47700c0
"Sandro" <"sandro007@gmail.com">
2022-07-15 00:09:26 -0300

    test

---
+++ contracts/add.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+contract add {
+
+function addf(uint x , uint y) external pure returns (uint){
+
+return x + y;
+
+}
+
+
+}
