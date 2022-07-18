0674b18723b18e87a6d6d6a12f3439da9f7a50c0
"Sandro" <"sandro007@gmail.com">
2022-07-17 23:58:57 -0300

    TEST

---
+++ contracts/transferether.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract sendether {
+
+constructor() payable {}
+
+function sendviatransfer(address payable _to) external payable {
+
+
+
+
+}
+
+
+
+
+
+
+}
